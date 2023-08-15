b09c6e30791c9291c3dcbe636a92c9c26119348b
WixMak <88415615+WixMak@users.noreply.github.com>
2023-08-15 23:18:05 +1000

    update

--- myprofile/src/App.js
+++ myprofile/src/App.js
@@ -1,22 +1,29 @@
-import './App.css';
+import React from "react";
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Navbar from "./components/Navbar";
+import Main from "./page/main";
+import Experience from "./page/Experience";
+import Services from "./page/Services";
+import Contact from "./page/Contact";
+
 
 function App() {
+    document.body.style.background = '#282c34'
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+          <Navbar />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/exp" element={<Experience />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<Main />} />
+            </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
